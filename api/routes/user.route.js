53d77e13a632047dcdceaf5cecb302c8f3eae748
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2023-12-28 15:07:57 +0530

    create test api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+import { test } from "../controller/user.controller.js";
+
+
+const router = express.Router();
+
+router.get('/test', test);
+
+export default router;
+ 
