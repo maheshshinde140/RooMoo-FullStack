681b39322ac7e68bedd5f04e8e079fef3109b92b
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2023-12-29 15:11:50 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import signN from "../assets/auth/signin.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormdData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormdData({
       ...formData,
@@ -16,30 +18,25 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json',
-      },
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-     console.log(data);
-     if(data.success === false){
-      setLoading(false);
-      setError(data.message);
-      return;
-     }
-     setLoading(false);
-     setError(null);
-     navigate("/");
-  } catch (error){
-    setLoading(false);
-    setError(error.message);
-  }
-    
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
   };
-    
+
   return (
     <section className="flex items-center justify-center space-x-1 lg:space-x-40 max-w-6xl mx-auto">
       {/* left img */}
@@ -47,8 +44,8 @@ export default function SignIn() {
         <img src={signN} alt="img" className="w-[500px] h-[420px] mt-9" />
       </div>
 
-      <form onSubmit={handleSubmit}> 
-        <div className="bg-[#F0FDF4] border-[3px] mt-9 w-[450px] md:w-[450px] lg:w-[500px] h-[480px] rounded-[0.6rem] shadow-lg">
+      <form onSubmit={handleSubmit}>
+        <div className="bg-[#F0FDF4] border-[3px] mt-9 w-[350px] md:w-[450px] lg:w-[500px] h-[480px] rounded-[0.6rem] shadow-lg">
           <div className=" font-bold text-white text-[25px]  bg-[#ed5012] rounded-[0.4rem]  h-[48px] ">
             <p className="mx-4 p-1 font-bold text-[25px]">Sign In</p>
           </div>
@@ -58,7 +55,7 @@ export default function SignIn() {
               Email or Mobile phone Number
             </span>
             <input
-              className="w-[400px] lg:w-[430px] mt-2 px-4 py-2 text-xl text-gray-800 bg-white border-gray-300 rounded transition ease-out"
+              className="w-[400px] lg:w-[430px] mt-2 px-4 py-2 text-xl text-gray-800 bg-white border-[1px] border-gray-400 rounded transition ease-out"
               type="email"
               id="email"
               onChange={handleChange}
@@ -70,14 +67,14 @@ export default function SignIn() {
               Password
             </span>
             <input
-              className="w-[400px] lg:w-[430px] mt-2 px-4 py-2 text-xl text-gray-800 bg-white border-gray-400 rounded transition ease-out"
+              className="w-[400px] lg:w-[430px] mt-2 px-4 py-2 text-xl text-gray-800 border-[1px] bg-white border-gray-400 rounded transition ease-out"
               type="password"
               id="password"
               onChange={handleChange}
             />
           </div>
 
-          <div className="flex justify-between px-4 mt-11 lg:text-[15px]  text-[13px] ">
+          <div className="flex justify-between px-4 mt-14 lg:text-[15px]  text-[13px] ">
             <p>
               Don't Have an Account?
               <Link
@@ -101,11 +98,11 @@ export default function SignIn() {
           <div className="flex justify-center flex-wrap">
             <button
               disabled={loading}
-              className="w-[410px] lg:w-[440px] text-white font-medium text-sm bg-blue-600 px-7 py-2 mt-4 rounded-sm shadow-sm hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 uppercase
+              className="w-[420px] lg:w-[440px] text-white font-medium text-sm bg-blue-600 px-7 py-2 mt-4 rounded-sm shadow-sm hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 uppercase
         "
               type="submit"
             >
-              {loading ? 'Loading...' : 'Sign In'}
+              {loading ? "Loading..." : "Sign In"}
             </button>
             <div
               className="flex items-center  my-3 before:border-t  before:flex-1 before:border-gray-300 
