c95d0b8a0ac7a218ab85589ff3c690c4139c5716
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2023-12-31 17:44:00 +0530

    complete update listing funtionality

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -12,7 +12,7 @@ export const createListing = async (req, res, next) => {
 
 
 export const deleteListing = async (req, res, next) => {
-  const listing = await Listing.findById(req.params._id);
+  const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
     return next(errorHandler(404, 'Listing not found! 😢'));
@@ -23,7 +23,7 @@ export const deleteListing = async (req, res, next) => {
   }
 
   try {
-    await Listing.findByIdAndDelete(req.params._id);
+    await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted! 😒');
   } catch (error) {
     next(error);
@@ -46,6 +46,18 @@ export const updateListing = async (req, res, next) => {
       { new: true }
     );
     res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/privateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
@@ -19,9 +20,8 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path='/update-listing/:listingId' element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
