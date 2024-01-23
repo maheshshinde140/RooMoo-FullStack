f566b8876d792b1eda81e3c9b7ad5d9d34635f7b
Mahesh Shinde <97420827+maheshshinde140@users.noreply.github.com>
2024-01-23 22:42:49 +0530

    navbar edited

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
           <div className=" ">
             <Link to="/profile">
               {currentUser ? (
-                <img className=" rounded-full h-9 w-9 object-cover border-[2px] shadow-sm hover:shadow-black border-black" src={currentUser.avatar} alt="profile" />
+                <img className=" rounded-full h-9 w-9 object-cover border-[2px] shadow-sm  border-black hover:border-[#f35313]" src={currentUser.avatar} alt="profile" />
               ):( <button className=" bg-[#dc4a11] p-2 mr-2 text-white rounded-full px-4 ml-4 font-bold hover:bg-[#f35313] transition duration-150 ease-in-out cursor-pointerp-[1px]">Sign In</button>
               ) }
             </Link>
